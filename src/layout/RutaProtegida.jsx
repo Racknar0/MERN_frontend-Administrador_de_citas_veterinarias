822bb2802981d7685aea646038356d3d6ebaa0db
Racknar0 <racknarow1@gmail.com>
2022-03-14 18:50:16 -0500

    Deployment FrontEnd

---
+++ src/layout/RutaProtegida.jsx
@@ -0,0 +1,27 @@
+import { Outlet, Navigate } from 'react-router-dom'
+
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+import useAuth from '../hooks/useAuth'
+
+const RutaProtegida = () => {
+
+    const { auth, cargando } = useAuth()
+
+    if( cargando ) return 'Cargando...'
+  return (
+
+    <>
+        <Header />
+            {auth?._id ? (
+                    <main className='container mx-auto mt-10'>
+                        <Outlet />
+                    </main>
+                    ) : <Navigate to='/'/>}
+        <Footer />
+    </>
+  )
+}
+
+export default RutaProtegida
---
+++ src/paginas/OlvidePassword.jsx
@@ -0,0 +1,76 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom'
+import Alerta from '../components/Alerta'
+import clienteAxios from '../config/axios'
+
+const OlvidePassword = () => {
+    const [email, setEmail] = useState('')
+    const [alerta, setAlerta] = useState({})
+
+
+    const handleSubmit = async e => {
+        e.preventDefault()
+
+        if ( email === '' || email.length < 6) {
+            setAlerta({msg: 'El email es Obligatorio', error: true})
+            return
+        }
+
+        try {
+            
+            const { data } = await clienteAxios.post('/veterinarios/olvide-password', { email })
+
+            console.log( data );
+
+            setAlerta({msg: data.msg})
+
+        } catch (error) {
+            setAlerta({
+                msg: error.response.data.msg,
+                error: true
+            })
+        }
+
+
+    }
+
+    const {msg} = alerta
+
+    return (
+        <>
+            
+            <div>
+                <h1 className="text-indigo-600 font-black text-6xl">
+                   Recupera tu acceso y no Pierdas{' '}
+                    <span className="text-black">tus Pacientes</span>
+                </h1>
+            </div>
+
+            <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
+
+            { msg && <Alerta 
+                    alerta={alerta} /> 
+            }
+
+                <form onSubmit={handleSubmit}>
+
+                    <div className='my-5'>
+                        <label htmlFor="" className='uppercase text-gray-600 block text-xl font-bold'>Email</label>
+                        <input type="email" placeholder='Email de registro' className='border w-full p-3 mt-3 bg-gray-50 rounded-xl' value={email} onChange={ e => setEmail(e.target.value)} />
+                    </div>
+
+                    <input type="submit" value="Enviar Instrucciones" className='bg-indigo-700 w-full py-3 px-10 rounded-lg text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto '/>
+                </form>
+                <nav className='mt-10 lg:flex lg:justify-between'>
+                    <Link className='block text-center my-5 text-gray-500' to="/">¿Ya tienes una cuenta? Inicia Sesión</Link>
+                    <Link className='block text-center my-5 text-gray-500' to="/registrar">¿No tienes una cuenta? Regístrate</Link>
+                </nav>
+            </div>
+
+            
+            
+        </>
+    );
+};
+
+export default OlvidePassword;
